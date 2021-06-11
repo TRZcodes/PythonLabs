6dd4f3d89e7be832a6ae17b194fca153893a669c
Jakub Trześniewski <82219314+TRZcodes@users.noreply.github.com>
2021-06-11 11:40:35 +0200

    Create Lab9.py

---
+++ Lab9/Lab9.py
@@ -0,0 +1,22 @@
+import modLab9
+
+#zad1
+print('------------zad1-------------')
+
+modLab9.PascTri(7)
+
+#zad2
+print('------------zad2-------------')
+modLab9.EulerTri(10)
+
+#zad3
+print('------------zad3-------------')
+modLab9.MakeCaesar(3)
+
+#zad4
+print('------------zad4-------------')
+modLab9.ReadCaesar(3)
+
+#zad5
+print('------------zad5-------------')
+modLab9.FreqTrans('CezarOut.txt')
